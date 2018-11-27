b970b14ea7f4b684f90fec9d9f29c96bca64ea01
Matheus Couto <couto.matheus@outlook.com>
2018-11-27 20:35:00 -0200

    style: centering components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { ExamplePage } from './pages';
+import styled from './styles/styled-components';
 
 const App: React.FunctionComponent = () => {
   
   return (
-    <div>
+    <AppWrapper>
       <Route exact path='/' component={ExamplePage} />
-    </div>
+    </AppWrapper>
   );
 }
 
 export default App;
+
+const AppWrapper = styled.div({
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
