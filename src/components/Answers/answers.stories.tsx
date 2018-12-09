cdd63bf57a07f987ad8e71bbdcb29283d65c4ab9
Matheus Couto <couto.matheus@outlook.com>
2018-12-09 09:33:48 -0200

    storybook: removing stories temporarily

---
+++ src/components/Answers/answers.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import OpenQuestion from './Open';
+import MultipleChoiceQuestion from './MultipleChoice';
+
+storiesOf('Answers', module)
+  // .add('open', () => <OpenQuestion/>)
+  // .add('multiple choice', () => <MultipleChoiceQuestion/>);
+  // .add('multiple choice', () => <MultipleChoiceQuestion/>);
---
+++ src/components/Question/question.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// import { storiesOf } from '@storybook/react';
+
+// import Wording from './';
+
+// storiesOf('Questions', module)
+  // .add('wording', () => <Wording>Pergunta 1</Wording>);
