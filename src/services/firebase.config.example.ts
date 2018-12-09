5419962d033171199cffd1fa2c12f643a15390e7
Matheus Couto <couto.matheus@outlook.com>
2018-12-09 09:35:56 -0200

    removing boilerplate

--- src/services/firebase.config.example.ts
+++
@@ -1,8 +0,0 @@
-export default {
-	apiKey: "AIzaSyCe9XJ---EXAMPLE---SQFBe20s",
-	authDomain: "PROJECT-NAME.firebaseapp.com",
-	databaseURL: "https://PROJECT-NAME.firebaseio.com",
-	projectId: "PROJECT-NAME",
-	storageBucket: "PROJECT-NAME.appspot.com",
-	messagingSenderId: "XXXXXXXXXXXX"
-};
